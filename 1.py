e48fc60444f301b0956eedaa3e5adbc98bc24cde
rafffffa221 <raffafahrezi0602@gmail.com>
2025-05-21 12:22:11 +0700

    contoh program perulanagan

---
+++ 1.py
@@ -0,0 +1,7 @@
+# Meminta input dari pengguna
+jumlah = int(input("Masukkan jumlah Text: "))
+
+# Perulangan for untuk mencetak teks sebanyak jumlah yang dimasukkan
+for i in range(jumlah):
+    print("Algoritma dan Pemrograman")
+    
